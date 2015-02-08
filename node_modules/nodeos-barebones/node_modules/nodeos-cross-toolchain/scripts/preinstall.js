308e4d56da16317b1748b64294a3f7f33afca6be
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-02-08 22:36:28 +0100

    Working on using 'download' package (WiP)

--- node_modules/nodeos-barebones/node_modules/nodeos-cross-toolchain/scripts/preinstall.js
+++ node_modules/nodeos-barebones/node_modules/nodeos-cross-toolchain/scripts/preinstall.js
@@ -3,44 +3,32 @@
 var Download = require('download');
 var progress = require('download-status');
 
-var eachSeries = require('async').eachSeries
-
 
 // Source versions
 
 const BINUTILS_VERSION = "2.24"
-const GCC_VERSION      = "4.9.2"
-const GLIBC_VERSION    = "2.20"
+const GCC_VERSION      = "4.7.3"  // "4.9.2"
+const LINUX_VERSION    = "3.18.3"
+const MUSL_VERSION     = "1.0.4"
 
 
 // Source URLs
 
 const BINUTILS_URL = "http://ftpmirror.gnu.org/binutils/binutils-"+BINUTILS_VERSION+".tar.gz"
 const GCC_URL      = "http://ftpmirror.gnu.org/gcc/gcc-"+GCC_VERSION+"/gcc-"+GCC_VERSION+".tar.gz"
-const GLIBC_URL    = "http://ftpmirror.gnu.org/glibc/glibc-"+GLIBC_VERSION+".tar.gz"
+const LINUX_URL    = "https://www.kernel.org/pub/linux/kernel/v3.x/linux-"+LINUX_VERSION+".tar.xz"
+const MUSL_URL     = "http://www.musl-libc.org/releases/musl-"+MUSL_VERSION+".tar.gz"
 
 
-function fetch(options, callback)
+Download({ extract: true, strip: 1 })
+.get(BINUTILS_URL, 'deps/binutils')
+.get(GCC_URL,      'deps/gcc')
+.get(LINUX_URL,    'deps/linux')
+.get(MUSL_URL,     'deps/musl')
+.use(progress())
+.run(function(error)
 {
-  var download = new Download({ extract: true, strip: 1 })
-      .get(options.url)
-      .dest(options.dest)
-      .use(progress());
-
-  download.run(callback);
-}
-
-
-var downloads =
-[
-  {url: BINUTILS_URL, dest: 'deps/binutils'},
-  {url: GCC_URL,      dest: 'deps/gcc'},
-  {url: GLIBC_URL,    dest: 'deps/glibc'}
-]
-
-eachSeries(downloads, fetch, function(error)
-{
-  if(error) throw err;
+  if(error) throw error;
 
   console.log('Download finished!');
 })
