8be56b5e024382ded54b36385c6569792e04fe0b
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-11-26 11:09:07 +0100

    Split runners so don't need to escape all special characters

--- resources/runners/iso.sh
+++ resources/runners/iso.sh
@@ -1,16 +1,12 @@
-#!/usr/bin/env sh
-
-QEMU=qemu-system-$CPU_FAMILY
-
 grep -q -e "vmx" -e "svm" /proc/cpuinfo
-if [ \$? -eq 0 ]; then
-  QEMU=\"\$QEMU -enable-kvm\"
+if [ $? -eq 0 ]; then
+  QEMU="$QEMU -enable-kvm"
 fi
 
-\$QEMU                  \\
-  -vga std              \\
-  -m 256M               \\
-  -redir tcp:50080::80  \\
-  -redir tcp:50443::443 \\
-  -hda rootfs           \\
+$QEMU                   \
+  -vga std              \
+  -m 256M               \
+  -redir tcp:50080::80  \
+  -redir tcp:50443::443 \
+  -hda rootfs           \
   -hdb usersfs
--- resources/runners/qemu.sh
+++ resources/runners/qemu.sh
@@ -1,18 +1,14 @@
-#!/usr/bin/env sh
-
-QEMU=qemu-system-$CPU_FAMILY
-
 grep -q -e "vmx" -e "svm" /proc/cpuinfo
-if [ \$? -eq 0 ]; then
-  QEMU=\"\$QEMU -enable-kvm\"
+if [ $? -eq 0 ]; then
+  QEMU="$QEMU -enable-kvm"
 fi
 
-\$QEMU                  \\
-  -vga std              \\
-  -m 256M               \\
-  -redir tcp:50080::80  \\
-  -redir tcp:50443::443 \\
-  --kernel barebones    \\
-  --initrd initramfs    \\
-  -hda     usersfs      \\
-  -append  \"root=/dev/sda ip=dhcp vga=0x344\"
+$QEMU                   \
+  -vga std              \
+  -m 256M               \
+  -redir tcp:50080::80  \
+  -redir tcp:50443::443 \
+  --kernel barebones    \
+  --initrd initramfs    \
+  -hda     usersfs      \
+  -append  "root=/dev/sda ip=dhcp vga=0x344"
