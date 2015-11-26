53dc9c706fab110eda806581fcb212e7ac788356
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-11-26 15:51:55 +0100

    Put in common the autodiscover CPU on nodeos-barebones

---
+++ node_modules/nodeos-barebones/scripts/processArguments.js
@@ -0,0 +1,50 @@
+var readlinkSync = require('fs').readlinkSync
+
+var minimist = require('minimist')
+
+
+function processArguments(argv)
+{
+  var opts =
+  {
+    string: 'cpu',
+    default:
+    {
+      cpu: readlinkSync('out/latest').split('/')[-2]
+    },
+    '--': true
+  }
+  var args = minimist(argv, opts)
+
+  var cpu = args.cpu
+
+  var cpu_family;
+  switch(cpu)
+  {
+    case 'armv6':
+      cpu_family = 'arm'
+    break
+
+    case 'i386':
+    case 'i486':
+    case 'i586':
+    case 'i686':
+    case 'i786':
+      cpu_family = 'i386'
+    break
+
+    case 'nocona':
+    case 'x86_64':
+      cpu_family = 'x86_64'
+    break
+
+    default:
+      console.error('Unknown cpu:',cpu)
+      process.exit(-1)
+  }
+
+  return {cpu: cpu, cpu_family: cpu_family, argv: args['--'] || args._}
+}
+
+
+module.exports = processArguments
