92b9fa2b94b61bb8c1f1ac8746f9b9439030b7d4
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-06-04 21:47:55 +0200

    Prepare `nodeos-initramfs` to be an independent layer

--- node_modules/nodeos-initramfs/scripts/utils.sh
+++ node_modules/nodeos-initramfs/scripts/utils.sh
@@ -1,9 +1,9 @@
-NPMi='CC=$TARGET-gcc             \
-      CXX=$TARGET-g++            \
-      npm_config_prefix=$OBJ_DIR \
-      $NODE_DIR/deps/npm/cli.js  \
-        install                  \
-        --quiet --no-spin        \
-        --arch=$NODE_ARCH        \
-        --nodedir=$NODE_DIR      \
-        --jobs=$JOBS             '
+NPMi='CC=$TARGET-gcc              \
+      CXX=$TARGET-g++             \
+      npm_config_prefix=$STEP_DIR \
+      $NODE_DIR/deps/npm/cli.js   \
+        install                   \
+        --quiet --no-spin         \
+        --arch=$NODE_ARCH         \
+        --nodedir=$NODE_DIR       \
+        --jobs=$JOBS              '
