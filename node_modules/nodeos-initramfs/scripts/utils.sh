4ee634611aa3e54b9b3e454cd9e0aa6af85103b0
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-06-12 01:58:40 +0200

    Fix location of prebuild Node.js binary

--- node_modules/nodeos-initramfs/scripts/utils.sh
+++ node_modules/nodeos-initramfs/scripts/utils.sh
@@ -1,8 +1,7 @@
 NPMi='CC=$TARGET-gcc              \
       CXX=$TARGET-g++             \
       npm_config_prefix=$STEP_DIR \
-      $NODE_DIR/deps/npm/cli.js   \
-        install                   \
+      npm install                 \
         --quiet --no-spin         \
         --arch=$NODE_ARCH         \
         --nodedir=$NODE_DIR       \
