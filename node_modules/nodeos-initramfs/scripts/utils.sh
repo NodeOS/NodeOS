ce23574107bcbfb64cf1e0d280d330f5b51e61f3
Jesús Leganés Combarro 'piranna <piranna@gmail.com>
2016-06-17 19:08:51 +0200

    Removed `node_modules` (don't know why they have appear...)

--- node_modules/nodeos-initramfs/scripts/utils.sh
+++
@@ -1,8 +0,0 @@
-NPMi='CC=$TARGET-gcc              \
-      CXX=$TARGET-g++             \
-      npm_config_prefix=$STEP_DIR \
-      npm install                 \
-        --quiet --no-spin         \
-        --arch=$NODE_ARCH         \
-        --nodedir=$NODE_DIR       \
-        --jobs=$JOBS              '
